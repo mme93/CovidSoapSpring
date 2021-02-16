04d17fc92a57cfe8fc4f65d73c691196ef1bafda
magantus@googlemail.com <Extrem93!>
2021-02-16 20:11:43 +0100

    new update

--- src/main/java/hrw/verteiltesysteme/CovidSoapSpring/CovidSoapRepository.java
+++ src/main/java/hrw/verteiltesysteme/CovidSoapSpring/CovidSoapRepository.java
@@ -6,47 +6,56 @@ import hrw.verteiltesysteme.CovidSoapSpring.covid.JHU;
 import hrw.verteiltesysteme.CovidSoapSpring.covid.RKI;
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
+import org.yaml.snakeyaml.emitter.ScalarAnalysis;
 
 
 @Component
 public class CovidSoapRepository {
 
 
-
+    //nDays und rValue kann Null sein
     public Covid findEmployee(int nDays, String info, int rValue) {
         CalculateCovidNumber calculateCovidNumber = new CalculateCovidNumber(new RKI().getRKICovidInfo(), new JHU().getJHUCovidInfo());
+        Covid covid;
         switch (info) {
             case "/date":
-                Covid covid = new Covid();
+                covid = new Covid();
                 covid.setJsonInfo(new JSONObject(calculateCovidNumber.getGermanyInfoJHU().get(calculateCovidNumber.getGermanyInfoJHU().size()-1).getDate()).toString());
-                return covid;   /*
-                case "/infection":
-                new Covid().setJsonInfo(new JSONObject(calculateCovidNumber.getGermanyInfoJHU().get(calculateCovidNumber.getGermanyInfoJHU().size()-1).getDate().toString());
-                return "Es gab " + new JSONObject(request.getNewInfection()).get("value")
-                        + " Neuinfektionen in den letzten 24 Stunden.";
+                return covid;
+            case "/infection":
+                covid = new Covid();
+                covid.setJsonInfo(calculateCovidNumber.getNewInfectionsLastDayJHU()+"");
+                return covid;
             case "/infected":
-                return "Die Gesamtcoronainfektionen liegen bei " + new JSONObject(request.getTotalInfection()).get("value")
-                        + " Menschen.";
+                covid = new Covid();
+                covid.setJsonInfo(calculateCovidNumber.getTotalInfectionsJHU()+"");
+                return covid;
             case "/increase":
-                return "Der prozentuale Anstieg der letzten 24 Stunden liegt bei "
-                        + new JSONObject(request.getPercenteInfection()).get("value") + "%.";
+                covid = new Covid();
+                //muss behoben werden
+                covid.setJsonInfo(calculateCovidNumber.getIncreaseLasteDayJHU()+"");
+            return covid;
             case "/average":
-                return "Der Anstieg in den letzten 7 Tagen beträgt "
-                        + new JSONObject(request.getAverageIncreaseDay(7)).get("value");
+                covid = new Covid();
+                covid.setJsonInfo(calculateCovidNumber.getAverageIncreaseDayJHU(7)+"");
+                return covid;
             case "/incidencevalue":
-                return "Der Inzidenzwert für Deutschland liegt aktuell bei: "
-                        + new JSONObject(request.getRWerthTotalGermany()).get("value");
+                covid = new Covid();
+                covid.setJsonInfo(calculateCovidNumber.getRWerthTotalGermanyRKI()+"");
+                return covid;
             case "/incidencegoal":
-                return "Der Ziel-Inzidenzwert ist " + new JSONObject(request.getTotalTargetInfection(35)).get("value")
-                        + ".";
+                covid = new Covid();
+                covid.setJsonInfo(calculateCovidNumber.getTotalTargetInfectionRKI(35)+"");
+                return covid;
             case "/days":
-                return "Es dauert aktuell " + new JSONObject(request.getTargetIncidenceForRWert(35, 7)).get("value")
-                        + "Tage um den Ziel-Inzidenzwert zu erreichen.";
-            */
+                covid = new Covid();
+                covid.setJsonInfo(calculateCovidNumber.getTargetIncidenceForRWerthRKI(35,calculateCovidNumber.getTotalInfectionsJHU(),7)+"");
+                return covid;
+
             default:
-                Covid s = new Covid();
-                s.setJsonInfo("Tut mir leid, diesen Befehl verstehe ich nicht.");
-                return s;
+                covid = new Covid();
+                covid.setJsonInfo("Tut mir leid, diesen Befehl verstehe ich nicht.");
+                return covid;
         }
     }
 }
